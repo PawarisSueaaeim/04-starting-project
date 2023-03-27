50b79b3df463a743d55ba312dab42d89461f8bbb
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-03-27 11:34:30 +0700

    test demo2

--- src/test/Syncchonuns.test.tsx
+++ src/test/Syncchonuns.test.tsx
@@ -1,9 +1,18 @@
 import { render, screen } from "@testing-library/react";
 import Demo1 from "../components/Demo1";
+import Demo2 from "../components/Demo2";
 
-test('render component Demo1', () => {
-    render(<Demo1 />);
-  
-    const divElement = screen.getByText(/pleum/i);
-    expect(divElement).toBeInTheDocument();
-  });
+test("render component Demo1", () => {
+  render(<Demo1 />);
+
+  const divElement = screen.getByText(/pleum/i);
+  expect(divElement).toBeInTheDocument();
+});
+
+test("render component Demo2", () => {
+  render(<Demo2 />);
+
+  expect(screen.getByTestId("button")).toBeDisabled();
+  expect(screen.getByTestId("input")).toBeDisabled();
+  expect(screen.getByTestId("button2")).not.toBeDisabled();
+});
