cd10f18eab778021f2d500f717b078b9a5784b36
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-03-27 11:54:44 +0700

    demo4 toContainElement

--- src/test/Syncchonuns.test.tsx
+++ src/test/Syncchonuns.test.tsx
@@ -2,6 +2,7 @@ import { render, screen } from "@testing-library/react";
 import Demo1 from "../components/Demo1";
 import Demo2 from "../components/Demo2";
 import Demo3 from "../components/Demo3";
+import Demo4 from "../components/Demo4";
 
 test("render component Demo1", () => {
   render(<Demo1 />);
@@ -24,4 +25,16 @@ test("render component Demo3", () => {
   expect(screen.getByTestId("empty")).toBeEmptyDOMElement();
   expect(screen.getByTestId("not-empty")).not.toBeEmptyDOMElement();
   expect(screen.getByTestId("with-whitespace")).not.toBeEmptyDOMElement();
+});
+
+test("render component Demo4", () => {
+  render(<Demo4 />);
+
+  const ancestor = screen.getByTestId("ancestor");
+  const descendant = screen.getByTestId("descendant");
+  const nonExistantElement = screen.getByTestId("does-not-exist");
+
+  expect(ancestor).toContainElement(descendant);
+  expect(descendant).not.toContainElement(ancestor);
+  expect(ancestor).not.toContainElement(nonExistantElement);
 });
