ec4085be7fa985620404a039e60177cb6cfeae2f
PawarisSueaaeim <pawaris.sueaaeim@gmail.com>
2023-03-27 11:44:03 +0700

    demo3 toBeEmptyDOMElement

--- src/test/Syncchonuns.test.tsx
+++ src/test/Syncchonuns.test.tsx
@@ -1,6 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import Demo1 from "../components/Demo1";
 import Demo2 from "../components/Demo2";
+import Demo3 from "../components/Demo3";
 
 test("render component Demo1", () => {
   render(<Demo1 />);
@@ -15,4 +16,12 @@ test("render component Demo2", () => {
   expect(screen.getByTestId("button")).toBeDisabled();
   expect(screen.getByTestId("input")).toBeDisabled();
   expect(screen.getByTestId("button2")).not.toBeDisabled();
+});
+
+test("render component Demo3", () => {
+  render(<Demo3 />);
+
+  expect(screen.getByTestId("empty")).toBeEmptyDOMElement();
+  expect(screen.getByTestId("not-empty")).not.toBeEmptyDOMElement();
+  expect(screen.getByTestId("with-whitespace")).not.toBeEmptyDOMElement();
 });
